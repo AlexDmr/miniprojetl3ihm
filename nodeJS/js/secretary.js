8bddf760813968eae2f726792a7ef8b25b28b3cb
AlexD <alxdmr2@yahoo.fr>
2016-09-28 21:44:53 +0200

    delete

--- nodeJS/js/secretary.js
+++
@@ -1,8 +0,0 @@
-var ControlPanel = require("./ControlPanelInteraction.js").ControlPanel
-  , domReady     = require("./domReady");
-
-domReady( function() {
-			 console.log(ControlPanel);
-			 ControlPanel.init( 'map', 'Affectations', 'newPatientMap' );
-			}
-		);
